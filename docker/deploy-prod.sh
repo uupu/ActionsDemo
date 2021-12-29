e981c0150a98de38961ac8b976f83c81e2d5989b
qianguangfu <qianguangfu1994@sina.com>
2021-12-29 23:58:14 +0800

    online

---
+++ docker/deploy-prod.sh
@@ -0,0 +1,27 @@
+#! bin/bash
+# 外部传给内部的变量脚本
+
+app_name="actions"
+port="3000"
+tag="latest"
+aliyun_image_registry="registry.cn-shanghai.aliyuncs.com/prod-terry"
+
+echo "对容器存活进行判断"
+if test -n "$(docker ps -a |grep $app_name)" ; then
+  echo "停止并且删除容器"
+  docker stop $app_name
+  docker rm $app_name
+  docker rmi $aliyun_image_registry/$app_name:$tag
+else
+  echo "未检查到$app_name容器运行"
+fi
+
+echo "获取最新的镜像"
+docker pull $aliyun_image_registry/$app_name:$tag
+echo "启动服务"
+# 有需要添加的环境变量随时添加
+docker run -itd\
+ -e TZ="Asia/Shanghai"\
+ -e SERVER_PORT=$port\
+ -p $port:$port\
+ --name $app_name  $aliyun_image_registry/$app_name:$tag
